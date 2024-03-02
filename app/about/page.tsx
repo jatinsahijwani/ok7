d7e89f034058c61613cc28763df709ebe7aa22d9
jatinsahijwani <117177197+jatinsahijwani@users.noreply.github.com>
2024-03-03 02:27:14 +0530

    minor changes (80% project)

---
+++ app/about/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "@/components//ui/sticky-scroll-reveal";
+import Image from "next/image";
+import { deflate } from "zlib";
+ 
+const content = [
+  {
+    title: "Mr. Neil Sharma",
+    description:
+      "A visionary Founder & Tech Lead mastering Python, AI & ML, and Cloud technologies, crafting transformative solutions with unparalleled expertise and innovation.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white text-center">
+        Founder & Tech-Lead
+      </div>
+    ),
+  },
+  {
+    title: "Mr. Arjun Srivastava",
+    description:
+      "Encounter a pioneering Co-founder & Tech Lead specialized in Blockchain, Web3, and JavaScript, architecting groundbreaking solutions at the forefront of technological evolution.",
+    content: (
+      <div className="h-full w-full text-center flex items-center justify-center text-white">
+        Co-founder & Tech-Lead
+      </div>
+    ),
+  },
+  {
+    title: "Mr. Ram Chaturvedi",
+    description:
+      "Step into the realm of innovation with Mr. Ram Chaturvedi, a dynamic Project Manager excelling in MERN/MEAN, PHP, and DevOps, orchestrating seamless project execution with strategic prowess and technical finesse.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white text-center">
+        Project Manager
+      </div>
+    ),
+  },
+  {
+    title: "Mr. Shaurya Shrivastav",
+    description:
+      "Dive into the forefront of innovation with our exceptional Creative Lead & Business Developer, revolutionizing industries through mastery in UI/UX, Graphic Design, and Sales. Experience visionary creativity and strategic acumen driving unparalleled growth.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white text-center">
+        Creative Lead <br /> & <br /> Business Developer
+      </div>
+    ),
+  },
+];
+
+export default function StickyScrollRevealDemo() {
+  return (
+    <div className="absolute w-[100vw] h-[100vh] top-0 left-0 bg-black">
+    <div className="p-5 mt-16">
+      <StickyScroll content={content} />
+    </div>
+    </div>
+  );
+}
---
+++ components/ui/HomeNavbar.jsx
@@ -0,0 +1,77 @@
+"use client";
+import React,{useState} from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import Image from "next/image";
+
+
+
+export default function HomeNavbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [
+    "Home",
+    "Career",
+    "About",
+    "Technologies",
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} className="fixed top-0 left-0">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand className="gap-4">
+        <Link color="foreground" href="/">
+        <img src="https://i.ibb.co/yNJ23mH/ok7logo.png" alt="ok7logo"
+          className="w-8 h-8"
+        />
+          {/* <p className="font-bold text-white text-inherit">Technical Services</p> */}
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Career
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/about">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/technologies">
+            Technologies
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Get Certificate
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
