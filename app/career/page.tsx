5a4ba330961ca8be22a36e8cfacf8b6df671e2c8
jatinsahijwani <117177197+jatinsahijwani@users.noreply.github.com>
2024-03-03 21:05:38 +0530

    integration success

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,13 +1,37 @@
 "use client";
+import { ChangeEvent } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/input";
 import { cn } from "@/utils/cn";
 
 export function  SignupFormDemo() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [firstName,setFirstName] = useState('');
+  const [lastName,setLastName] = useState('');
+  const [email,setEmail] = useState('');
+  const [mobile,setMobile] = useState('');
+  const [resume,setResume] = useState<File | null>(null);
+  const [role,setRole] = useState('');
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+    const formData = new FormData();
+    formData.append('firstName',firstName);
+    formData.append('lastName',lastName);
+    formData.append('email',email);
+    formData.append('role',role);
+    formData.append('mobile',mobile);
+    if(resume) formData.append('resume',resume);
+    const response = await fetch('http://localhost:4500/api/save-details',{
+      method: 'POST',
+      body: formData
+    });
+    if(response.ok) console.log("great job");
+    else console.log("we fucked up");
   };
+  const handleResumeSubmit = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setResume(e.target.files[0]);
+    }
+  }
   return (
     <div style={{
       background:
@@ -24,26 +48,32 @@ export function  SignupFormDemo() {
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="John" type="text" />
+            <Input value={firstName} onChange={(e) => {setFirstName(e.target.value)}} id="firstname" placeholder="John" type="text" />
           </LabelInputContainer>
           <LabelInputContainer>
             <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Doe" type="text" />
+            <Input id="lastname" placeholder="Doe" type="text" value={lastName} onChange={(e)=> {setLastName(e.target.value)}} />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="john.dev@gmail.com" type="email" />
+          <Input id="email" value={email} onChange={(e) => {
+            setEmail(e.target.value)
+          }} placeholder="john.dev@gmail.com" type="email" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="mobile">Mobile</Label>
-          <Input id="mobile" placeholder="+91 999888222" type="text" />
+          <Input value={mobile} onChange={(e) => {setMobile(e.target.value)}} id="mobile" placeholder="+91 999888222" type="text" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
-          <Label htmlFor="Message">Message (Optional)</Label>
+          <Label htmlFor="Role">Role</Label>
           <Input
-            id="message"
-            placeholder="Message"
+            id="role"
+            value={role}
+            onChange={(e) => {
+              setRole(e.target.value)
+            }}
+            placeholder="Role"
             type="text"
           />
         </LabelInputContainer>
@@ -53,6 +83,7 @@ export function  SignupFormDemo() {
             id="resume"
             placeholder="Resume"
             type="file"
+            onChange={handleResumeSubmit}
             accept=".pdf"           
           />
         </LabelInputContainer>
@@ -92,7 +123,7 @@ const LabelInputContainer = ({
   );
 };
 
-import React from "react";
+import React, { useState } from "react";
 import { SparklesCore } from "@/components/ui/sparkles";
 
 export default function SparklesPreview() {
